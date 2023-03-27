8f05d8db57585e5d5f06d2f59a50cdd1a9d146c0
majkipl <majkipl@gmail.com>
2023-03-27 16:53:10 +0200

    adding a transaction if for some reason it would throw an error while saving the data

--- app/Services/CurrencyService.php
+++ app/Services/CurrencyService.php
@@ -16,21 +16,28 @@ class CurrencyService
 
         if ($currencies) {
             foreach ($currencies[0]->rates as $rate) {
-                $currency = Currency::where('currency_code', $rate->code)->first();
+                DB::beginTransaction();
 
-                // 2. check if currency exists in db
-                if ($currency) {
-                    // 3. then update
-                    $currency->exchange_rate = $rate->mid;
-                    $currency->save();
-                } else {
-                    // 4. else save
-                    Currency::create([
-                        'id' => \Illuminate\Support\Str::uuid(),
-                        'name' => $rate->currency,
-                        'currency_code' => $rate->code,
-                        'exchange_rate' => $rate->mid,
-                    ]);
+                try {
+                    $currency = Currency::where('currency_code', $rate->code)->first();
+
+                    // 2. check if currency exists in db
+                    if ($currency) {
+                        // 3. then update
+                        $currency->exchange_rate = $rate->mid;
+                        $currency->save();
+                    } else {
+                        // 4. else save
+                        Currency::create([
+                            'id' => \Illuminate\Support\Str::uuid(),
+                            'name' => $rate->currency,
+                            'currency_code' => $rate->code,
+                            'exchange_rate' => $rate->mid,
+                        ]);
+                    }
+                    DB::commit();
+                } catch (\Exception $e) {
+                    DB::rollback();
                 }
             }
         }
