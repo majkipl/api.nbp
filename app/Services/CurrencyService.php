12363c10bf0f7d7467e84bf9ce8dd48e5d09c53b
majkipl <majkipl@gmail.com>
2023-03-27 16:30:28 +0200

    using the previously created integration, I check whether it has downloaded the data

--- app/Services/CurrencyService.php
+++ app/Services/CurrencyService.php
@@ -2,11 +2,17 @@
 
 namespace App\Services;
 
+use App\Integrations\NbpApiIntegration;
+
 class CurrencyService
 {
     public function getCurrencies()
     {
         // 1. get data from api : example using integration
+        $nbpApi = new NbpApiIntegration();
+        $currencies = $nbpApi->getExchangeRates();
+
+        dd($currencies);
 
         // 2. check if currency exists in db
         // 3. then update
