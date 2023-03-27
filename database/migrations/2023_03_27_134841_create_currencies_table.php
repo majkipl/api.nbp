afddf9c9712f1cf55d72e050e2d72ee71abc2131
majkipl <majkipl@gmail.com>
2023-03-27 15:55:39 +0200

    create migration for Currency

---
+++ database/migrations/2023_03_27_134841_create_currencies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name',128);
+            $table->string('currency_code',3)->index();
+            $table->decimal('exchange_rate', 5, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('currencies');
+    }
+};
