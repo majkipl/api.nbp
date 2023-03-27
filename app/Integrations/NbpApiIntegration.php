40b954b90e6e11a0b65bab2903145451e7fb0de4
majkipl <majkipl@gmail.com>
2023-03-27 17:01:32 +0200

    I'm refactoring the code in NbpApiIntegration

--- app/Integrations/NbpApiIntegration.php
+++ app/Integrations/NbpApiIntegration.php
@@ -3,13 +3,20 @@
 namespace App\Integrations;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 
 class NbpApiIntegration
 {
-    public function getExchangeRates()
+    private const NBP_API_URL = 'http://api.nbp.pl/api/exchangerates/tables/a/';
+
+    /**
+     * @return mixed
+     * @throws GuzzleException
+     */
+    public function getExchangeRates(): mixed
     {
         $client = new Client();
-        $response = $client->request('GET', 'http://api.nbp.pl/api/exchangerates/tables/a/' );
+        $response = $client->request('GET', self::NBP_API_URL );
 
         if ($response->getStatusCode() == 200) {
             return json_decode($response->getBody());
