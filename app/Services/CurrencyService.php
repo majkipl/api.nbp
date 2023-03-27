dfb34d3c0b0682ac49260be1736e3e3b7084ef25
majkipl <majkipl@gmail.com>
2023-03-27 16:17:31 +0200

    draw plan actions

--- app/Services/CurrencyService.php
+++ app/Services/CurrencyService.php
@@ -6,6 +6,10 @@ class CurrencyService
 {
     public function getCurrencies()
     {
+        // 1. get data from api : example using integration
 
+        // 2. check if currency exists in db
+        // 3. then update
+        // 4. else save
     }
 }
